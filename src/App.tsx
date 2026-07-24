95427490127300614253036c4ee54702aad9d814
待看云开 <xyk953651094@qq.com>
2026-07-24 21:09:34 +0800

    1、此插件将不再提供更新

--- src/App.tsx
+++ src/App.tsx
@@ -71,9 +71,9 @@ function App() {
                 notification.open({
                     icon: null,
                     message: "已更新至版本 V" + currentVersion,
-                    description: "详细内容请前往菜单栏更新日志查看",
+                    description: "此插件后续不再更新，请前往插件商店搜索 云开诗词新标签页",
                     placement: "bottomLeft",
-                    duration: 5,
+                    duration: 10,
                     closeIcon: false
                 });
                 setExtensionStorage("SkyNewTabPoemReactVersion", currentVersion);
